999bcb0e06218d437d7c5fc50d33f4ebd97cf7cd
Jake White <jake.white@teamalabaster.com>
2016-05-03 17:39:41 -0700

    Custom Nav Menu Items

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once dirname( __FILE__ ) . '/menu-item-custom-fields/menu-item-custom-fields.php';
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
